ef8ee022b6cf4ee632a3f6623d53debdad263195
Claudio Salvati <140422208+ClaudioSalvati@users.noreply.github.com>
2023-08-05 17:55:17 +0200

    Update Convert Komoot JSON files to CSV.py

--- Convert Komoot JSON files to CSV.py
+++ Convert Komoot JSON files to CSV.py
@@ -9,6 +9,10 @@ input_folder = "<your input folder>"
 # Define the output folder where the CSV files will be saved
 output_folder = "<your output folder>"
 
+# Function to check if a key exists in a dictionary and return its value if present, else return a default value
+def get_value_with_default(data_dict, key, default_value):
+    return data_dict.get(key, default_value)
+
 # Function to sanitize the name and replace forbidden characters with "_"
 # In case you have used characters in the tours name that are forbidden in file names (I did)
 def sanitize_name(name):
@@ -20,7 +24,7 @@ json_files = [file for file in os.listdir(input_folder) if file.endswith(".json"
 # Iterate through each JSON file
 for json_file in json_files:
     # Skip the file if it has the name "komoot_data.json"
-    # this file is generated in the script "Get Komoot tour JSON data.py" and should be converted to CSV. I stored it in the same folder and needed to skip it
+    # this file is generated in the script "Get Komoot tour JSON data.py" and should not be converted to CSV. I stored it in the same folder and needed to skip it
     if json_file == "komoot_data.json":
         continue
 
@@ -35,8 +39,11 @@ for json_file in json_files:
     df_selected["activity_date"] = pd.to_datetime(df_selected["date"]).dt.strftime("%Y-%m-%d").str[:10]
 
     # Extract additional data from the JSON using .loc to add two new columns: "vector_map_image_src" and "map_image_src"
-    df_selected.loc[:, "vector_map_image_src"] = df["vector_map_image"]["src"]
-    df_selected.loc[:, "map_image_src"] = df["map_image"]["src"]
+    vector_map_image_src = get_value_with_default(df, "vector_map_image", {}).get("src", "")
+    map_image_src = get_value_with_default(df, "map_image", {}).get("src", "")
+
+    df_selected.loc[:, "vector_map_image_src"] = vector_map_image_src
+    df_selected.loc[:, "map_image_src"] = map_image_src
 
     # Create a new DataFrame without duplicate rows
     df_selected_unique = df_selected.drop_duplicates()
@@ -53,7 +60,7 @@ for json_file in json_files:
 
     # Sanitize the name to replace any forbidden characters with "_"
     name = sanitize_name(name)
-
+    # Build file path
     csv_file = f"{output_folder}{activity_date}_{name}_tour_info.csv"
 
     # Save the table to the CSV file without row indexes
